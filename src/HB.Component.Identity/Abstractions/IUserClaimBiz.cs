c11d8cacca049d53c0e8d27ba22c1168a7a068bd
yzbai <yzbai@brlite.com>
2019-07-10 18:08:58 +0800

    xxx

---
+++ src/HB.Component.Identity/Abstractions/IUserClaimBiz.cs
@@ -0,0 +1,15 @@
+﻿using HB.Component.Identity.Entity;
+using HB.Framework.Database;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.Component.Identity.Abstractions
+{
+    internal interface IUserClaimBiz
+    {
+        Task<IList<UserClaim>> GetAsync(string userGuid, TransactionContext transContext = null);
+    }
+}
